ecf2974eb317c76fa522149512de8ad439321c59
Lorhan Sohaky <contato+gi74@lorhan.me>
2023-02-20 14:44:24 -0300

    feat: :children_crossing: handle exception and show to user

--- src/wsl_manager/main.py
+++ src/wsl_manager/main.py
@@ -3,14 +3,18 @@ import views
 
 
 def main():
-    option = views.display_menu()
+    try:
+        option = views.display_menu()
 
-    if option == "shrink":
-        controllers.shrink()
-    elif option == "move":
-        controllers.move()
-    else:
-        raise ValueError("Invalid option")
+        if option == "shrink":
+            controllers.shrink()
+        elif option == "move":
+            controllers.move()
+        else:
+            raise ValueError("Invalid option")
+    except Exception:
+        views.console.print_exception(show_locals=True)
+        views.press_any_key_to_continue()
 
 
 if __name__ == "__main__":
